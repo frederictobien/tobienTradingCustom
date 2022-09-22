cdedcdda846d77348d7571c69d1bc84d29c18144
lasalesi <larsericmueller@gmail.com>
2022-09-22 15:56:47 +0200

    Update Purchase Invoice.js
    extended Purchase invoice script: fetch receipt date

--- tobientrading_custom/fixtures/custom_scripts/Purchase Invoice.js
+++ tobientrading_custom/fixtures/custom_scripts/Purchase Invoice.js
@@ -8,5 +8,34 @@ frappe.ui.form.on("Purchase Invoice", {
             });
 
         }
+    },
+    refresh: function(frm) {
+        if (frm.doc.items) {
+            check_fetch_delivery_dates(frm);
+        }
     }
-})
+});
+
+function check_fetch_delivery_dates(frm) {
+    // loop through items
+    for (var i = 0; i < frm.doc.items.length; i++) {
+        // check if there is a purchase receipt and no delivery date
+        if ((frm.doc.items[i].purchase_receipt) && (!frm.doc.items[i].delivery_date)) {
+            // fetch delivery date from purchase receipt
+            frappe.call({
+                "method": "frappe.client.get",
+                "args": {
+                    "doctype": "Purchase Receipt",
+                    "name": frm.doc.items[i].purchase_receipt
+                },
+                "async": false,
+                "callback": function(response) {
+                    var purchase_receipt = response.message;
+                    if (purchase_receipt) {
+                        frappe.model.set_value(cur_frm.doc.items[i].doctype, cur_frm.doc.items[i].name, "delivery_date", purchase_receipt.posting_date);
+                    } 
+                }
+            });
+        }
+    }
+}
